ae220b0d86ff2c29dde2bf9571d80d62388a4c0a
arthur <arthurfleck8@proton.me>
2026-02-26 12:03:27 +0100

    nix: update server crontab

--- nixos/hosts/ax-fuji/configuration.nix
+++ nixos/hosts/ax-fuji/configuration.nix
@@ -66,10 +66,10 @@
     cron = {
       enable = true;
       systemCronJobs = [
-        "10 3 * * *     ax     . /etc/profile; /usr/bin/env bb $HOME/x/backup/ax-srv-backup-radicale.clj >> ~/cron-radicale.log 2>&1"
-        "12 3 * * *     ax     . /etc/profile; ruby $HOME/x/backup/ax-srv-backup-linkding.rb >> ~/cron-linkding.log 2>&1"
+        "10 3 * * *     ax     . /etc/profile; /usr/bin/env bb $HOME/x/backup/ax_srv_radicale.clj >> ~/cron-radicale.log 2>&1"
+        "12 3 * * *     ax     . /etc/profile; /usr/bin/env bb $HOME/x/backup/ax_srv_linkding.clj >> ~/cron-linkding.log 2>&1"
         "15 3 * * *     ax     . /etc/profile; ruby $HOME/x/backup/ax-srv-backup-immich.rb >> ~/cron-immich.log 2>&1"
-        "25 3 * * *     ax     . /etc/profile; ruby $HOME/x/backup/ax-srv-rclone-b2.rb >> ~/cron-rclone-b2.log 2>&1"
+        "25 3 * * *     ax     . /etc/profile; /usr/bin/env bb $HOME/x/backup/ax_srv_rclone_b2.clj >> ~/cron-rclone-b2.log 2>&1"
         # download daily wallpaper
         "0 9,10,11 * * *     ax     . /etc/profile; nix develop ~/x --command ruby ~/x/bing_wallpaper_dl.rb >> ~/bing.log 2>&1"
       ];
