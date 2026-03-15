7826beac99d057fb170452cd3f7c8264dd5331bb
arthur <arthurfleck8@proton.me>
2026-03-15 23:24:07 +0100

    fix vpn script

--- scripts/openbsd/vpn.sh
+++ scripts/openbsd/vpn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-WGIF="$(ifconfig wg0 2>/dev/null | grep -Eo '[A-Z]{2}-[0-9]+')"
+WGIF="$(ifconfig wg0 2>/dev/null | grep -Eo '[A-Z]{2}-[0-9]+|muc')"
 
 if [ -z "$WGIF" ]; then
     echo "NO VPN"
