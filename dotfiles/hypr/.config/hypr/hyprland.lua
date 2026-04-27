17e733cec3aefe07c1e65ed043d028b6ca4cd6f2
arthur <arthurfleck8@proton.me>
2026-04-27 11:34:38 +0200

    hypr: configure monitors

--- dotfiles/hypr/.config/hypr/hyprland.lua
+++ dotfiles/hypr/.config/hypr/hyprland.lua
@@ -16,10 +16,16 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
-    scale    = "auto",
+    output   = "DP-1",
+    mode     = "3840x2160",
+    position = "0x0",
+    scale    = 1.25,
+})
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "1920x1080",
+    position = "3072x648",
+    scale    = 1,
 })
 
 
