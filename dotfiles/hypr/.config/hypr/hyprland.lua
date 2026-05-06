742ce16ecc85f930d22e35a95aed67b7880ff183
arthur <arthurfleck8@proton.me>
2026-05-06 09:18:52 +0200

    hypr: add more screenshot keybinds

--- dotfiles/hypr/.config/hypr/hyprland.lua
+++ dotfiles/hypr/.config/hypr/hyprland.lua
@@ -420,9 +420,14 @@ end)
 -- submap screenshot
 hl.bind(mainMod .. "+ CTRL + S", hl.dsp.submap("screenshot"))
 hl.define_submap("screenshot", "reset", function()
+    -- screenshot selection, copy to clipboard
+    hl.bind("C",         function() hl.exec_cmd('grim -g "$(slurp)" - | wl-copy && notify-send -t 2500 "Screenshot copied to clipboard"') end)
+    -- screenshot all outputs, save directly to disk
+    hl.bind("P",         function() hl.exec_cmd('FILE="$HOME/sync/screenshots/2026/$(date +%Y%m%d-%H%M%S)_screenshot.png"; grim "$FILE" && notify-send "Screenshot saved" "$FILE"') end)
+    -- swappy (annotation tool)
     -- screenshot all
     hl.bind("A",         function() hl.exec_cmd("grim - | swappy -f -") end)
-    -- screenshot select
+    -- screenshot selection
     hl.bind("S",         function() hl.exec_cmd('grim -g "$(slurp)" - | swappy -f -') end)
     -- screenshot window
     hl.bind("W",         function() hl.exec_cmd("$HOME/x/hypr-screenshot-window.sh") end)
