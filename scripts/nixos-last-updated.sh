ea746164f584e4bb833fbc03625e1857a5578463
arthur <arthurfleck8@proton.me>
2026-05-19 13:16:09 +0200

    dotfiles/scripts: add last nixos update time to fastfetch

---
+++ scripts/nixos-last-updated.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+cd ~/syscfg/nixos-config || exit 1
+date=$(git log -1 --pretty=%ci -- flake.lock)
+[ -z "$date" ] && exit 1
+
+echo "$date" | awk '
+{
+  split($1, d, "-")
+  dstr = d[3]"."d[2]". "substr($2,1,5)
+  cmd = "date -d \""$0"\" +%s"
+  cmd | getline ts
+  close(cmd)
+  now = systime()
+  age = now - ts
+  printf "%s — \033[32m%dd %dh ago\033[0m\n", dstr, age/86400, age%86400/3600
+}'
+
