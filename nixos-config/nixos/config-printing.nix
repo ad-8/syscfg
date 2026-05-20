9f199bb888d3b2c75e58422cd2da271c688c453d
arthur <arthurfleck8@proton.me>
2026-05-20 18:13:53 +0200

    nixos: fix printer config by using ipp everywhere

--- nixos-config/nixos/config-printing.nix
+++ nixos-config/nixos/config-printing.nix
@@ -7,18 +7,22 @@
 
 {
   options = {
-    configPrinting.enable = lib.mkEnableOption "Enable CUPS and install printing pkgs";
+    configPrinting.enable = lib.mkEnableOption "Enable auto-discovery of network printers (IPP Everywhere)";
   };
 
   config = lib.mkIf config.configPrinting.enable {
-
-    services.printing = {
-      enable = true; # Enable CUPS to print documents.
-      # drivers = [ pkgs.hplip ]; # printer works w/ or w/o this
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
     };
 
-    environment.systemPackages = with pkgs; [
-      system-config-printer
-    ];
+    services.printing = {
+      enable = true;
+      drivers = with pkgs; [
+        cups-filters
+        cups-browsed
+      ];
+    };
   };
 }
