2d1ddb8ebfbd8a4d8952b865c0dbee0f52ed2277
arthur <arthurfleck8@proton.me>
2026-05-27 12:32:46 +0200

    nnn: improve config on wayland

--- dotfiles/fish/.config/fish/nnn.fish
+++ dotfiles/fish/.config/fish/nnn.fish
@@ -1,9 +1,18 @@
 # Source this file in config.fish !
 
 # Bookmarks: example from wiki -> spaces in path are ok
-export NNN_BMS=".:$HOME/.config;c:$HOME/my/code;d:$HOME/Downloads/;h:$HOME;m:$HOME/my;s:$HOME/sync"
+set --export NNN_BMS ".:$HOME/.config;c:$HOME/my/code;d:$HOME/Downloads/;h:$HOME;m:$HOME/my;s:$HOME/sync"
+
 # Plugins
 set --export NNN_FIFO "/tmp/nnn.fifo"
-export NNN_PLUG='P:preview-tui;p:preview-tabbed;F:fzcd;f:fzopen;'
-# preview-tui uses this
-export NNN_TERMINAL="foot"
+
+set _nnn_common 'F:fzcd;f:fzopen;g:getplugs;'
+if set -q WAYLAND_DISPLAY
+    set --export NNN_PLUG "p:preview-tui;P:preview-tabbed;$_nnn_common"
+    set --export NNN_TERMINAL "foot"
+else
+    set --export NNN_PLUG "p:preview-tabbed;P:preview-tui;$_nnn_common"
+end
+
+# preview-tui img program
+set --export NNN_PREVIEWIMGPROG "chafa"
