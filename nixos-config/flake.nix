e3e26a7838ce807ef5ee66630a50413ee7c2454f
arthur <arthurfleck8@proton.me>
2026-06-12 12:09:25 +0200

    nixos: bump hyprland

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -12,7 +12,7 @@
     #   url = "github:nix-community/home-manager/master";
     #   inputs.nixpkgs.follows = "nixpkgs-unstable";
     # };
-    hyprland.url = "github:hyprwm/Hyprland/v0.55.3";
+    hyprland.url = "github:hyprwm/Hyprland/v0.55.4";
   };
 
   outputs =
