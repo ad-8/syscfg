7588421ee0758965a061236aef5f42e3095942f8
arthur <arthurfleck8@proton.me>
2026-08-22 15:38:11 +0200

    nixos/ax-bee: back to sway for now
    hyprland won't build, river/kwm has no scratchpad,
    niri neither, what does a man do then

--- nixos-config/hosts/ax-bee/configuration.nix
+++ nixos-config/hosts/ax-bee/configuration.nix
@@ -15,7 +15,7 @@
   configWorkstation.enable = true;
   configFirefox.enable = true;
   configRiver.enable = true;
-  configHyprland.enable = true;
+  configSway.enable = true;
   configAudio.enable = true;
   configClamav.enable = true;
   configGaming.enable = true;
