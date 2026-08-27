95b2340497d5f888323ad5f32e30441d5e6a9f9f
arthur <arthurfleck8@proton.me>
2026-08-27 08:54:57 +0200

    nixos: update river config

--- nixos-config/nixos/config-river.nix
+++ nixos-config/nixos/config-river.nix
@@ -32,6 +32,7 @@
         };
 
         environment.systemPackages = with pkgs; [
+          kitty # use kitten thing as scratchpad replacement
           river
           gammastep
           swayidle
